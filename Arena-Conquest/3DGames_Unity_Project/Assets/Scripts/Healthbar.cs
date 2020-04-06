fc4a47df9c37f7331374d908e6626012f7c50cc0
mohamed shiha <46888852+S00189636@users.noreply.github.com>
2020-04-07 00:35:13 +0100

    Before Artifact folder delete
    Removed the walls
    changed the star speed

--- Arena-Conquest/3DGames_Unity_Project/Assets/Scripts/Healthbar.cs
+++ Arena-Conquest/3DGames_Unity_Project/Assets/Scripts/Healthbar.cs
@@ -10,14 +10,19 @@ public class Healthbar : MonoBehaviour
     Vector3 barX = Vector3.one;
     private void Start()
     {
-        barX.x = (Health.currentHealth / Health.FullHealth);
         Health.OnTakingDamage += Health_OnTakingDamage;
     }
+
 
     private void Health_OnTakingDamage(float amount)
     {
         barX.x -= (amount / Health.FullHealth) ;
         barX.x = Mathf.Clamp(barX.x, -0.001f, Health.FullHealth);
         bar.transform.localScale = barX;
+    }
+
+    private void OnDisable()
+    {
+        Health.OnTakingDamage -= Health_OnTakingDamage;
     }
 }
